a893134aa685594e774968d607e376cbbf854fa4
ButuzaCalin <67749327+ButuzaCalin@users.noreply.github.com>
2022-01-27 23:12:46 +0200

    Delete AJAXphp directory

--- AJAXphp/database.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $db_name = "droid";
-    $mysql_user = "test";
-    $mysql_pass = "test";
-    $server_name = "localhost";
-    $con = mysqli_connect($server_name, $mysql_user, $mysql_pass, $db_name);
